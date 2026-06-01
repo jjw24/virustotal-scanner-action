8bde532c76cf6d840cb579b40017b4e5ec5c7938
Jeremy <jeremy24wu@gmail.com>
2026-06-01 23:14:38 +1000

    add path handling for debugging

--- virustotal_scan/__main__.py
+++ virustotal_scan/__main__.py
@@ -1,5 +1,10 @@
 """Allow ``python -m virustotal_scan`` as an entry point."""
 
+import sys
+from pathlib import Path
+
+sys.path.insert(0, str(Path(__file__).resolve().parent.parent))
+
 from virustotal_scan.cli import main
 
 if __name__ == "__main__":
